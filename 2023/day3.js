7ab356da8fd6e62f9f4d516be2c9fa5ea39fd170
Antonio <antoniojaramillofanta@gmail.com>
2023-12-11 21:01:39 +0100

    sth

--- 2023/day3.js
+++ 2023/day3.js
@@ -4,38 +4,49 @@ const fs = require('fs');
 let input = fs.readFileSync('./inputs/day3.txt').toString();
 let inputLines = input.split('\n');
 
-let map = [];
-let numbers = [];
-for (let i=0; i<inputLines.length; i++) {
+let validNumbers = [];
+
+function isValid(num){
+    let y = num.y;
+    const regex2 = /^[.]*$/;
+
+    const above = inputLines[y-1] ? inputLines[y-1].slice(num.x-1, num.x+num.length+1) : '.';
+    const below = inputLines[y+1] ? inputLines[y+1].slice(num.x-1, num.x+num.length+1) : '.';
+    const left = inputLines[y][num.x-1];
+    const right = inputLines[y][num.x+num.length];
+    
+    const testAbove = regex2.test(above);
+    const testBelow = regex2.test(below);
+    const testLeft = regex2.test(left);
+    const testRight = regex2.test(right);
+
+    if(!testAbove || !testBelow || !testLeft || !testRight){
+        return true;
+    }
+}   
+
+for (let i = 0; i < inputLines.length; i++) {
     const line = inputLines[i];
-    let regex = /\d+/g;
+    const regex1 = /\d+/g;
     let match;
-    while ((match = regex.exec(line)) !== null) {
-        const num = {
-            x: match.index,
+    let numbers = [];
+    while ((match = regex1.exec(line)) !== null) {
+        numbers.push({
+            number: parseInt(match[0]),
             y: i,
-            value: parseInt(match[0])
+            x: match.index,
+            length: match[0].length
+        })
+    }
+    
+    for (num of numbers){
+        if(isValid(num)){
+            validNumbers.push(num.number);
         }
-        numbers.push(num);
     }
 
-    for (let j=0; j<line.length; j++) {
-        const char = {x: i, y: j, value: line[j]};
-        map.push(char);
-    }
 }
+const sumOfValidNumbers = validNumbers.reduce((a, b) => a + b, 0);
+console.log(validNumbers);
+console.log(sumOfValidNumbers);
 
-// const numbers = []
-
-// function extractNumbersFromLine(map, x) {
-//     let line = map.filter(point => point.x === x);
-//     let str = line.map(point => point.value).join('');
-//     let numbers = str.match(/\d+/g);
-//     return numbers ? numbers.map(num => parseInt(num)) : [];
-// }
-
-
-
-// if (line[j] !== '.') {
-    // char.nextCoordinates =[{x: i+1, y: j}, {x: i, y: j+1}, {x: i-1, y: j}, {x: i, y: j-1}, {x: i+1, y: j+1}, {x: i-1, y: j-1}, {x: i+1, y: j-1}, {x: i-1, y: j+1}]
-// }
