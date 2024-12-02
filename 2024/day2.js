95acc5655f9583f0b24a477f598ae2c93c81c4cb
Antonio Jaramillo <antoniojaramillofanta@gmail.com>
2024-12-02 16:17:43 +0100

    part 2

--- 2024/day2.js
+++ 2024/day2.js
@@ -3,6 +3,14 @@ const fs = require('fs');
 const input = fs.readFileSync('./inputs/day2.txt').toString().replace(/\r/g, '');
 const lines = input.split('\n');
 const splitLines = lines.map(line => line.split(' ').map(Number));
+// const splitLines = [
+//   [7,6,4,2,1],
+//   [1,2,7,8,9],
+//   [9,7,6,2,1],
+//   [1,3,2,4,5],
+//   [8,6,4,4,1],
+//   [1,3,6,7,9]
+// ];
 
 function solution1(splitLines) {
   let safeReports = 0;
@@ -35,4 +43,40 @@ function solution1(splitLines) {
   return safeReports;
 }
 
+function solution2(splitLines) {
+  function isSafe(nums, errors = 0) {
+    const posDir = nums[1] > nums[0];
+    
+    for (let i = 1; i < nums.length; i++) {
+      const num = nums[i];
+      const prev = nums[i - 1];
+
+      if (
+        posDir
+        ? num <= prev || num - 3 > prev
+        : num >= prev || num + 3 < prev
+      ) {
+        if (errors === 1) {
+          return false;
+        }
+        const next1 = nums.slice(0, i - 1).concat(nums.slice(i));
+        const next2 = nums.slice(0, i).concat(nums.slice(i + 1));
+        const next3 = nums.slice(1);
+        return isSafe(next1, 1) || isSafe(next2, 1) || isSafe(next3, 1);
+      }
+    }
+    return true;
+  }
+
+  let safeReports = 0;
+  for (let line of splitLines) {
+    if (isSafe(line)) {
+      safeReports++;
+    }
+  }
+  return safeReports;
+}
+
 console.log('Solution 1: ', solution1(splitLines));
+console.log('Solution 2: ', solution2(splitLines));
+
