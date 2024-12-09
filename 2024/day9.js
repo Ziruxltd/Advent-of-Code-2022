ec79614d9a77d2d7b0de1aee11cb589e115bf622
Antonio Jaramillo Fanta <antoniojaramillofanta@gmail.com>
2024-12-09 21:49:46 +0100

    part 2

--- 2024/day9.js
+++ 2024/day9.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 const input = fs.readFileSync('./inputs/day9.txt').toString().replace(/\r/g, '');
-const map = '2333133121414131402'
 
 function solution1 (map) {
-  let idArray = map.split('').map((val,idx) => {
+  let idArray = map.split('').map((val, idx) => {
     if (idx % 2 === 0) {
       return Array(parseInt(val)).fill(idx / 2)
     } else {
       return Array(parseInt(val)).fill('.')
     }
-  }).flat()
+  }).flat().map(String);  
   
   let totalNumbers = idArray.reduce((total, current) => {
     if (current !== '.') {
@@ -40,32 +39,66 @@ function solution1 (map) {
 }
 
 function solution2(map) {
-  let idArray = map.split('').map((val,idx) => {
+  let idArray = map.split('').map((val, idx) => {
     if (idx % 2 === 0) {
       return Array(parseInt(val)).fill(idx / 2)
     } else {
       return Array(parseInt(val)).fill('.')
     }
-  }).flat()
-  console.log(idArray);
+  }).flat().map(String);
 
-  let currentNumber = null
-  let numberIdx = null
-  let numberLength = 0
-  for (let i = 0; i > idArray.length; i++) {
-
+  let number = null
+  let size = 0
+  let numIdx = null
+  for (let i = idArray.length -1; i >= 0; i--) {
+    if (!number) {
+      number = idArray[i]
+    }
+    if (number && number === idArray[i]) {
+      size++
+    }
+    if (number !== idArray[i-1]) {
+      numIdx = i
+    }
+    if (numIdx) {
+      let spaceIdx = null
+      let spaceSize = 0
+      for (let j = 0; j < numIdx; j++) {        
+        if (!spaceIdx && idArray[j] === '.') {
+          spaceIdx = j
+        }
+        if (idArray[j] === '.') {
+          spaceSize++
+        }
+        if (idArray[j+1] && idArray[j+1] !== '.') {
+          // test
+          if (spaceSize >= size) {
+            for ( let x = 0; x < size; x++) {              
+              idArray[spaceIdx + x] = number
+              idArray[numIdx + x] = '.'
+            }
+            number = null
+            size = 0
+            numIdx = null
+          } else {
+            spaceIdx = null
+            spaceSize = 0
+          }
+        }
+      }
+      number = null
+      size = 0
+      numIdx = null
+    }
   }
-
-
   let sum = 0
   for (let i = 0; i < idArray.length; i++) {
-    if (idArray[i] === '.') break
-    const result = i * parseInt(idArray[i])
+    const result = idArray[i] === '.' ? 0 : i * parseInt(idArray[i])
     sum += result
   }
 
   return sum
 }
 
-// console.log('Solution 1: ', solution1(input))
-console.log('Soluution 2: ', solution2(map));
+console.log('Solution 1: ', solution1(input))
+console.log('Soluution 2: ', solution2(input));
