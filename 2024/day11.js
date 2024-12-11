1662f60b6f0b86aa863539d01e4abc1573766438
Antonio Jaramillo <antoniojaramillofanta@gmail.com>
2024-12-11 12:44:16 +0100

    third try

--- 2024/day11.js
+++ 2024/day11.js
@@ -2,19 +2,37 @@ const stones = '3279 998884 1832781 517 8 18864 28 0'.split(' ');
 // const stones = '125 17'.split(' ');
 
 function solutions(stones, times, chunkSize = 1000) {
-  let rocks = [...stones];
+  let rocks = { 0: [...stones] };
+  let chunkIndex = 1;
 
   for (let i = 0; i < times; i++) {
-    let newRocks = [];
-    for (let j = 0; j < rocks.length; j += chunkSize) {
-      let chunk = rocks.slice(j, j + chunkSize);
-      let newChunk = processChunk(chunk);
-      newRocks = newRocks.concat(newChunk);
+    let newRocks = {};
+    let newChunkIndex = 0;
+
+    for (let key in rocks) {
+      let chunk = rocks[key];
+      for (let j = 0; j < chunk.length; j += chunkSize) {
+        let subChunk = chunk.slice(j, j + chunkSize);
+        let newChunk = processChunk(subChunk);
+
+        if (!newRocks[newChunkIndex]) {
+          newRocks[newChunkIndex] = [];
+        }
+        newRocks[newChunkIndex] = newRocks[newChunkIndex].concat(newChunk);
+
+        if (newRocks[newChunkIndex].length >= chunkSize) {
+          newChunkIndex++;
+        }
+      }
     }
+
     rocks = newRocks;
-    console.log('Loop:', i, 'rocks length:', rocks.length);
+    // let totalLength = Object.values(rocks).reduce((acc, chunk) => acc + chunk.length, 0);
+    console.log('Loop:', i);
   }
-  return rocks.length;
+
+  let finalLength = Object.values(rocks).reduce((acc, chunk) => acc + chunk.length, 0);
+  return finalLength;
 }
 
 function processChunk(chunk) {
