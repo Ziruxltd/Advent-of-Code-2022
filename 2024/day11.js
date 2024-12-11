a801cecc6cea78f2b8d384908fb92383d97b6808
Antonio Jaramillo <antoniojaramillofanta@gmail.com>
2024-12-11 12:06:28 +0100

    first

--- 2024/day11.js
+++ 2024/day11.js
@@ -5,25 +5,27 @@ function solutions(stones, times) {
   let rocks = [...stones];
 
   for (let i = 0; i < times; i++) {
+    let newRocks = []
     for (let j = 0; j < rocks.length; j++) {
       if (rocks[j] === '0') {
-        rocks[j] = '1';
+        newRocks.push('1');
       } else if (rocks[j].length % 2 === 0) {
         const digits = rocks[j].length / 2;
         const left = rocks[j].slice(0, digits);
         let right = Number(rocks[j].slice(digits)).toString();
 
-        rocks.splice(j, 1, left, right);
-        j++; // Skip the next element since it's already processed
+        newRocks.push(left)
+        newRocks.push(right)
       } else {
         const result = Number(rocks[j]) * 2024;
-        rocks[j] = result.toString();
+        newRocks.push(result.toString())
       }
     }
+    rocks = newRocks
     console.log('Loop:', i, 'rocks length:', rocks.length);
   }
   return rocks.length;
 }
 
-console.log('Solution 1:', solutions(stones, 25));
+// console.log('Solution 1:', solutions(stones, 25));
 console.log('Solution 2:', solutions(stones, 75));
